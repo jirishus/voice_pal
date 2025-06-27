47e3168fe3d5af61481ef35eb2b0f214686de87d
jirishus <jirishus@yahoo.com>
2025-06-26 23:26:40 -0700

    add questions endpoint

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from 'src/app.service';
 
 @Controller('questions')
-export class QuestionsController {}
+export class QuestionsController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getQuestions(): string {
+    return this.appService.getHello();
+  }
+}
