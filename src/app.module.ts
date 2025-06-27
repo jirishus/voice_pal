1d58ac419ee5015b1481b574162abcce9a40f425
jirishus <jirishus@yahoo.com>
2025-06-26 23:17:28 -0700

    question module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuestionProcessorModule } from './question-processor/question-processor.module';
+import { QuestionsController } from './questions/questions.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [QuestionProcessorModule],
+  controllers: [AppController, QuestionsController],
   providers: [AppService],
 })
 export class AppModule {}
